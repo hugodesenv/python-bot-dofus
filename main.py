f35f39c42813e678ca3a3ee6c1ad9f359049135e
Hugo Souza <hugo.souza@mbmsolutions.com.br>
2024-10-18 23:35:43 -0300

    Implementado esquema de coordenadas e correcoes de bugs de direcoes

--- main.py
+++ main.py
@@ -6,14 +6,16 @@ import time
 # objeto que armazena o caminho dos itens
 obj_listagem_recursos = [
   '/agua',
-  '/arvore',
+  '/arvore/arvore_freixo',
+  '/arvore/arvore_boldo',
+  '/arvore/arvore_castanheiro',
   '/planta/urtiga'
 ]
 
-# objeto que armazena em qual X e Y o ponteiro do mouse ficará para trocar de mapa:
+# objeto que armazena em qual X e Y o ponteiro do mouse ficarÃ¡ para trocar de mapa:
 obj_coordenadas_monitor = {
   'cima': {
-    'horizontal': 1300,
+    'horizontal': 1370,
     'vertical': 35,
     'direcoes_inversas': ['esquerda', 'direita']
   },
@@ -39,7 +41,7 @@ obj_coordenadas_monitor = {
   obj_mapa_mapeado = [
     [9, -25]{
       lados_bloqueados: ['direita', 'baixo'],
-      saidas: [[10, 2], [1, 3]]
+      saidas: [{'x': 10, 'y': 20}, {'x': 2, 'y': -3}]
     }
   ]
 '''
@@ -47,26 +49,58 @@ obj_coordenadas_monitor = {
 # objeto que guardamos o mapeamento do nosso mapa atual (lados que nao podem ser clicaveis etc)
 obj_mapa_mapeado = {}
 
-TEMPO_TROCA_MAPA = 7
+# coordenadas dos recursos a serem coletados (hoje estou trabalhando apenas com urtiga e agua, depois verificar se precisar de mais coisa)
+obj_coordenadas_recursos =  [
+  [1,-27], 
+  [1, -22], 
+  [2, -21], 
+  [2, -30], 
+  [3, -30], 
+  [3, -31], 
+  [3, -20], 
+  [4, -20], 
+  [4, -31], 
+  [5, -30], 
+  [5, -29], 
+  [6, -30], 
+  [6, -26], 
+  [7, -26], 
+  [7, -22], 
+  [8, -22], 
+  [7, -21], 
+  [7, -16], 
+  [1, -16], 
+  [1, -15], 
+  [4, -15], 
+  [3, -16], 
+  [3, -14], 
+  [9, -14], 
+  [10, -13], 
+  [3, -13], 
+  [3, -12], 
+  [11, -12], 
+  [9, -11], 
+  [3, -11]
+]
+
+TEMPO_TROCA_MAPA = 8
 
 # com base no caminho fornecido, verificamos se a imagem existe no mapa atual
-# eu coloquei um delay de 5 segundos quando nao encontrar a imagem pra forçar ele encontrar
-def checar_imagem_no_mapa(s_caminho_imagem):
-  b_achou = False
-  for i in range(5):
+# eu coloquei um delay de 5 segundos quando nao encontrar a imagem pra forcar ele encontrar
+def checar_imagem_no_mapa(s_caminho_imagem, f_confidence, i_qtde_repeticao, i_delay):
+  posicao = None
+  for i in range(i_qtde_repeticao):
     for imagem in os.listdir(s_caminho_imagem):
       try:
-        pyautogui.locateOnScreen(s_caminho_imagem + imagem, confidence=0.7)  
-        b_achou = True
-      except:
-        pass
-    
-    if b_achou: 
+        posicao = pyautogui.locateOnScreen(s_caminho_imagem + imagem, confidence=f_confidence)  
+      except: pass
+      
+    if posicao: 
       break
     
-    time.sleep(1)
+    time.sleep(i_delay)
     
-  return b_achou
+  return posicao
 
 # funcao que verifica se existe a seta de direcionamento no mapa
 def existe_seta_troca_mapa(pdirecao):
@@ -75,9 +109,9 @@ def existe_seta_troca_mapa(pdirecao):
   
   pyautogui.moveTo(x, y, 0.5)  
   
-  return checar_imagem_no_mapa('./img/seta/' + pdirecao + '/')
+  return checar_imagem_no_mapa('./img/seta/' + pdirecao + '/', 0.4, 2, 1)
 
-# funçao interna para definirmos a movimentação do personagem
+# funcao interna para definirmos a movimentacao do personagem
 def calcular_troca_mapa(pdirecao, i_pos_personagem_x, i_pos_personagem_y):
   if pdirecao == 'direita':
     i_pos_personagem_x += 1
@@ -99,42 +133,35 @@ def buscar_nova_direcao(x_ou_y_str, posicao_atual, posicao_destino):
   else:
     return ''
   
-# funcao que faz a coleta dos recursos no mapa atual
-def coletar_recurso(p_caminho_imagem):
-  s_caminho = './img' + p_caminho_imagem + '/'
-  if checar_imagem_no_mapa(s_caminho):
-    pyautogui.click()
-    time.sleep(6)
-    
 # funcao principal
 def main():
-  # o primeiro passo é testarmos se está calculando certo a movimentação do personagem
+  # o primeiro passo eh testarmos se esta calculando certo a movimentacao do personagem
   i_pos_personagem_x = int(input('X: '))
   i_pos_personagem_y = int(input('Y: '))
-  
-  i_pos_destino_x = 3
-  i_pos_destino_y = -17
-  
-  b_executando = True
-  
+
   time.sleep(4)
 
   #---------------------------------------------------------------------------------------------------------------------
-  # primeiro mapeamos o nosso mapa, verificando se ele existe lado que nao pode ser clicável etc
-  # guardando as informações no nosso objeto
+  # primeiro mapeamos o nosso mapa, verificando se ele existe lado que nao pode ser clicavel etc
+  # guardando as informacoes no nosso objeto
   def _key_pos_atual(): return (i_pos_personagem_x, i_pos_personagem_y)
   #---------------------------------------------------------------------------------------------------------------------                                                                                      
-  # funcao anonima para coletar os recursos
-  #def _coletar_recursos():
-  #  for caminho_imagem in obj_listagem_recursos:
-  #    coletar_recurso(caminho_imagem)
-  #---------------------------------------------------------------------------------------------------------------------
 
-  # laço principal pra aplicação não parar de rodar
-  # eu também passo dentro do for pra ele percorrer tanto na diagonal quanto na horizontal
-  while b_executando:    
+  # laco principal pra aplicacao nao parar de rodar
+  # eu tambem passo dentro do for pra ele percorrer tanto na diagonal quanto na horizontal
+  # nessa variavel indice_coord_destino eu guardo o index que estamos indo, e quando chegamos incrementamos + 1
+  indice_coord_destino = 0
+  
+  while True:    
     for X_Y in ['x', 'y']:
-      # faz o mapeamento das posições possiveis do mapa atual
+      # armazeno o proximo indice destino
+      if indice_coord_destino == len(obj_coordenadas_recursos):
+        indice_coord_destino = 0
+      
+      i_pos_destino_x = obj_coordenadas_recursos[indice_coord_destino][0]
+      i_pos_destino_y = obj_coordenadas_recursos[indice_coord_destino][1]
+
+      # faz o mapeamento das posiÃ§Ãµes possiveis do mapa atual
       if _key_pos_atual() not in obj_mapa_mapeado:
         obj_mapa_mapeado[_key_pos_atual()] = { 'lados_bloqueados': [], 'saidas': [] }   
         
@@ -142,10 +169,20 @@ def main():
           if not existe_seta_troca_mapa(s_direcao):
             obj_mapa_mapeado[_key_pos_atual()]['lados_bloqueados'].append(s_direcao)
 
-      # se chegamos no nosso destino, então buscamos uma nova rota
-      if (_key_pos_atual() == (i_pos_destino_x, i_pos_destino_y)):
-        b_executando = False
-        break
+      # coleta o recurso do mapa
+      for caminho in obj_listagem_recursos:
+        s_caminho_recurso = './img' + caminho + '/'
+        posicao = checar_imagem_no_mapa(s_caminho_recurso, 0.8, 1, 0)
+        if posicao:
+          pyautogui.moveTo(posicao[0] + 5, posicao[1] + 5)
+          pyautogui.click()
+          time.sleep(10)
+
+      # se chegamos no nosso destino, entao buscamos uma nova rota
+      if _key_pos_atual() == (i_pos_destino_x, i_pos_destino_y):
+        print("\nChegou no nosso destino...")
+        indice_coord_destino += 1
+        continue
 
       # descobrimos nossa proxima sugestao de direcao      
       s_direcao_sugestiva = ''
@@ -156,22 +193,24 @@ def main():
         s_direcao_sugestiva = buscar_nova_direcao('y', i_pos_personagem_y, i_pos_destino_y)
       
       if s_direcao_sugestiva == '':
+        print('\nDireção sugestiva está vazia...')
         continue
       
-      # se existe a seta para trocar de mapa, então vamos lá:
+      # se existe a seta para trocar de mapa, entao vamos la:
       if existe_seta_troca_mapa(s_direcao_sugestiva):
+        print('\nExiste a seta para trocarmos de mapa...' + s_direcao_sugestiva)
         i_pos_personagem_x, i_pos_personagem_y = calcular_troca_mapa(s_direcao_sugestiva, i_pos_personagem_x, i_pos_personagem_y)     
         
         pyautogui.click()
         time.sleep(TEMPO_TROCA_MAPA) 
       else:        
-        # vamos supor que queremos ir para a direita, e o nosso personagem não pode ir pra direita porque
-        # existe um muro que o impede. Nesse caso, vamos calcular a melhor saída pro mesmo, sendo assim,
-        # primeiro a gente sobe o personagem até encontrar uma saída, e depois descemos o personagem até encontrar uma saída
-        # após finalizar esses dois processos, voltamos o personagem pra posição que estava e decidimos o melhor.
-        # em alguns casos pode ser que nao exista nem chegando no limite da posiçao inversa, por exemplo:
+        # vamos supor que queremos ir para a direita, e o nosso personagem nÃ£o pode ir pra direita porque
+        # existe um muro que o impede. Nesse caso, vamos calcular a melhor saÃ­da pro mesmo, sendo assim,
+        # primeiro a gente sobe o personagem atÃ© encontrar uma saÃ­da, e depois descemos o personagem atÃ© encontrar uma saÃ­da
+        # apos finalizar esses dois processos, voltamos o personagem pra posicao que estava e decidimos o melhor.
+        # em alguns casos pode ser que nao exista nem chegando no limite da posiÃ§ao inversa, por exemplo:
         # supondo que eu quero achar a direita, entao tenho que subir, e subir, e subir... mas chega no final do mapa
-        # e nada acontece, então paramos o processo.
+        # e nada acontece, entao paramos o processo.
         direcoes_inversas = obj_coordenadas_monitor[s_direcao_sugestiva]['direcoes_inversas']
      
         for direcao in direcoes_inversas:
@@ -179,11 +218,10 @@ def main():
           while True:
             # se encontramos a saida, entao anotamos dentro do objeto
             # eu nunca olho o primeiro por causa do "sentido contrario" que nao funcionaria
-            # já que o personagem possivelmente parou numa saída, então devemos desconsidera-la
+            # ja que o personagem possivelmente parou numa saida, entao devemos desconsidera-la
             if i > 0 and existe_seta_troca_mapa(s_direcao_sugestiva):
-              #--> apenas tetse @Hugo
-              adicionar aqui, dentro do objeto da posiçao atual mapeada
-              exemplo...: append((i_pos_personagem_x, i_pos_personagem_y))
+              print('\nSaida encontrada...')
+              obj_mapa_mapeado[_key_pos_atual()]['saidas'].append((i_pos_personagem_x, i_pos_personagem_y))              
               break
 
             if existe_seta_troca_mapa(direcao):
@@ -193,22 +231,23 @@ def main():
               pyautogui.click()
               time.sleep(TEMPO_TROCA_MAPA)
             else: 
+              print('\nNada a se fazer!')
               break         
               
-        # caso contrário, se nao existir então vamos buscar a proxima saída, com isso guardamos seu posicionamento
+        # caso contrÃ¡rio, se nao existir entÃ£o vamos buscar a proxima saÃ­da, com isso guardamos seu posicionamento
         # da posicao atual do personagem para poder voltar posteriormente
         #index_proxima_saida = 0
         
-        # para controle de voltar o personagem na posição inicial ao mapear as saidas do mapa
+        # para controle de voltar o personagem na posiÃ§Ã£o inicial ao mapear as saidas do mapa
         #i_pos_personagem_inicial_x = i_pos_personagem_x
         #i_pos_personagem_inicial_y = i_pos_personagem_y
         
-        # percorremos entao as duas direçoes, pra depois decidir a mais proxima
-        # isso é feito apenas se nao conter o mapa mapeado já
+        # percorremos entao as duas direÃ§oes, pra depois decidir a mais proxima
+        # isso Ã© feito apenas se nao conter o mapa mapeado jÃ¡
         #while index_proxima_saida <= 1:
         #  s_nova_direcao = obj_coordenadas_monitor[s_direcao_sugestiva]['direcoes_inversas'][index_proxima_saida]
                 
-        #  print('\nCalculando nova direção: ' + s_nova_direcao)
+        #  print('\nCalculando nova direÃ§Ã£o: ' + s_nova_direcao)
         #  while ((i_pos_personagem_x != i_pos_personagem_inicial_x) or 
         #         (i_pos_personagem_y != i_pos_personagem_inicial_y)):                  
         #    if existe_seta_troca_mapa(s_nova_direcao):
